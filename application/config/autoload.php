1577046f4769ce10c31cabe2c9f57a8de223fa23
Tanggon <tanggonkalburochastu@gmail.com>
2019-11-28 06:46:13 +0700

    tes session

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('image_lib', 'form_validation');
+$autoload['libraries'] = array('image_lib', 'form_validation', 'session');
 
 /*
 | -------------------------------------------------------------------
