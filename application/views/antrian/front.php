f81b5902298ff11992e33d4fb4600288107649e5
Tanggon <tanggonkalburochastu@gmail.com>
2019-12-01 02:20:16 +0700

    layout antrian v2

--- application/views/antrian/front.php
+++ application/views/antrian/front.php
@@ -2,66 +2,95 @@
     <div class="container-fluid" style="max-width:90%">
         <div class="row">
             <div class="col-lg-12" style="background-color:white;">
-
-                <div class="row">
-                    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-                        <img src="<?= $asset ?>/images/logo.jpg" class="img-responsive" alt="Image" style="  max-width: 100%; height: auto;">
+                <div class="row" style="background-image: url('<?php echo $asset ?>/images/navbar.jpg');   background-repeat: no-repeat; background-size: 100% 100%; border-radius:5px">
+                    <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+                        <img src="<?= $asset ?>/images/logodjp.png" class="img-responsive" alt="Image" style="  max-width: 30%; height: auto;  ">
                     </div>
-                    <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9" style="  margin: auto;">
+                    <!-- <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9" style="  margin: auto;">
                         <h1><b>KPP PRATAMA BANYUWANGI</b></h1>
+                    </div> -->
+
+                    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                        <img src="<?= $asset ?>/images/logosahjak.png" class="img-responsive" alt="Image" style="  max-width: 30%; height: auto; float:right;margin:10px;">
                     </div>
+
+
+                    <div class="col-md-4" style="padding:0px; background-color:white; margin-top:10px; margin-bottom:10px; margin-left:-10px; border-radius:15px">
+
+                        <div class="row">
+                            <div class="col-md-12 antrian">
+                                <span style="background-color:lightgray; vertical-align:middle; font-weight:bold; font-size:60px;">
+                                    A-1</span>
+                                <span style="background-color:WHITE; vertical-align:middle; font-size:30px; font-weight:bold;  margin-left:30px">
+                                    LOKET 06
+                                </span>
+                                <hr style="width:80%; height: 6px; border:none; background-color:black; border-radius:5px; margin-top:5px">
+                            </div>
+                        </div>
+
+
+                    </div>
+
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4" style="padding:0px">
-                <div class="col-lg-12" style="background-color:lavender; height:125px; ">
-                    <p style="text-align:center; margin:0px">Antrian Sekarang:</p>
-                    <p style="font-size:60px; text-align:center; margin:0px">
-                        A-1
-                    </p>
-                    <p style="text-align:center; margin:0px">Antrian Selanjutnya:</p>
-                </div>
-                <div class="col-lg-12" style="background-color:lavender; height:125px; ">
-                    <p style="text-align:center; margin:0px">Antrian Sekarang:</p>
-                    <p style="font-size:60px; text-align:center; margin:0px">
-                        A-1
-                    </p>
-                    <p style="text-align:center; margin:0px">Antrian Selanjutnya:</p>
-                </div>
-                <div class="col-lg-12" style="background-color:lavender; height:125px; ">
-                    <p style="text-align:center; margin:0px">Antrian Sekarang:</p>
-                    <p style="font-size:60px; text-align:center; margin:0px">
-                        A-1
-                    </p>
-                    <p style="text-align:center; margin:0px">Antrian Selanjutnya:</p>
-                </div>
-                <div class="col-lg-12" style="background-color:lavender; height:125px; ">
-                    <p style="text-align:center; margin:0px">Antrian Sekarang:</p>
-                    <p style="font-size:60px; text-align:center; margin:0px">
-                        A-1
-                    </p>
-                    <p style="text-align:center; margin:0px">Antrian Selanjutnya:</p>
-                </div>
-            </div>
             <div class="col-md-8" style="height:500px; padding:0px">
-                <iframe style="height: 100%; width:100%" src="https://www.youtube.com/embed/videoseries?list=PLVYQIt6z-lj0Tw08Pr_iq-n4lnj2IbZiU&loop=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
+                <iframe style="height: 100%; width:100%;  background-color:white" src="https://www.youtube.com/embed/videoseries?list=PLVYQIt6z-lj0Tw08Pr_iq-n4lnj2IbZiU&loop=1&<span style=' color: #ff0000;'><strong>theme=light</strong></span>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
                 </iframe>
             </div>
+            <div class="col-md-4" style="padding:0px; background-color:white">
+
+                <div class="col-lg-12 antrian" style=" height:55px; ">
+                    <span class="" style="background-color:WHITE; vertical-align:middle; font-size:30px; font-weight:bold;  margin-left:30px">
+                        ANTRIAN SEKARANG
+                    </span>
+                </div>
+
+                <div class="col-lg-12 antrian" style=" height:80px; ">
+                    <span style="background-color:WHITE;  vertical-align:middle; font-weight:bold; font-size:60px;">
+                        A-1</span>
+                    <span style="background-color:lightgray;vertical-align:middle; font-size:40px; font-weight:bold;  margin-left:30px">
+                        LOKET 06
+                    </span>
+                </div>
+                <div class="col-lg-12 antrian" style=" height:80px; ">
+                    <span style="background-color:WHITE;  vertical-align:middle; font-weight:bold; font-size:60px;">
+                        A-1</span>
+                    <span style="background-color:lightgray; vertical-align:middle; font-size:40px; font-weight:bold;  margin-left:30px">
+                        LOKET 06
+                    </span>
+                </div>
+                <div class="col-lg-12 antrian" style=" height:80px; ">
+                    <span style="background-color:WHITE;  vertical-align:middle; font-weight:bold; font-size:60px;">
+                        A-1</span>
+                    <span style="background-color:lightgray; vertical-align:middle; font-size:40px; font-weight:bold;  margin-left:30px">
+                        LOKET 06
+                    </span>
+                </div>
+                <div class="col-lg-12 antrian" style=" height:80px; ">
+                    <span style="background-color:WHITE;  vertical-align:middle; font-weight:bold; font-size:60px;">
+                        A-1</span>
+                    <span style="background-color:lightgray; vertical-align:middle; font-size:40px; font-weight:bold;  margin-left:30px">
+                        LOKET 06
+                    </span>
+                </div>
+            </div>
+
         </div>
         <div class="row">
-            <div class="col-lg-12" style="background-color:blue; padding:30px">
+            <div class="col-lg-12" style="background-color:#2e325f; padding:15px; border-radius:10px;">
                 <marquee style="font-size:30px; text-align:center; margin:0px; color:white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</marquee>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-4 float-right" style="background-color:white; padding:30px">
+            <div class="col-lg-4 float-right" style="background-color:white; padding:10px">
                 <p style="font-size:20px; text-align:left; margin:0px"><b>POWERED BY SAHJAK</b></p>
             </div>
-            <div class="col-lg-4 float-right" style="background-color:white; padding:30px">
+            <div class="col-lg-4 float-right" style="background-color:white; padding:10px">
                 <p style="font-size:15px; text-align:center; margin:0px"></p>
             </div>
-            <div class="col-lg-4 float-right" style="background-color:white; padding:30px">
+            <div class="col-lg-4 float-right" style="background-color:white; padding:10px">
                 <p style="font-size:20px; text-align:right; margin:0px"><b>Copyright @SMARTDIGIPRO</b></p>
             </div>
         </div>
--- application/views/style/admin_header.php
+++ application/views/style/admin_header.php
@@ -29,6 +29,10 @@
          .btn {
              margin: 3px
          }
+
+         #antrian {
+             text-align: center;
+         }
      </style>
 
      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
